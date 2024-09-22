a1fd14482a9a0014194e4ace1ec6734b3b97b736
Dominic Ferrando <dominicf2001@gmail.com>
2024-09-22 18:18:52 -0400

    more polishing

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -3,12 +3,13 @@
     import { HttpTransportType, HubConnectionBuilder, type HubConnection } from "@microsoft/signalr";
     import dayjs from "dayjs";
     import "vidstack/bundle";
+    import type { MediaPlayerElement } from "vidstack/elements";
 
-    let connection: HubConnection;
     let songSession: SongSession | undefined; 
+    let player: MediaPlayerElement | undefined;
 
     $effect(()=>{  
-        connection = new HubConnectionBuilder()
+        const connection = new HubConnectionBuilder()
             .withUrl("http://192.168.4.29:5066/roomHub", {
                 skipNegotiation: true,
                 transport: HttpTransportType.WebSockets
@@ -25,24 +26,24 @@
 
         connection.start();
 
-        const player = document.querySelector("media-player");
-        if (player){
-            return player.subscribe(({ paused, canPlay }) => {
-                if (canPlay){
-                    if (paused) player.play();
+        const unsubPlayer = player?.subscribe(({ paused, canPlay }) => {
+            if (player && canPlay){
+                if (paused) player.play();
 
-                    player.currentTime = songSession ? 
-                        dayjs().diff(dayjs(songSession.startTime), "ms") / 1000 :
-                        0;
-                }            
-            });
+                player.currentTime = songSession ? 
+                    dayjs().diff(dayjs(songSession.startTime), "ms") / 1000 :
+                    0;
+            }            
+        });
+
+        return () => {
+            unsubPlayer?.();
+            connection.stop();
         }
-
-        return () => connection.stop();
     });
 </script>
 
-<media-player preload="auto" streamType="ll-live" muted={true}>
+<media-player bind:this={player} preload="auto" streamType="ll-live" muted={true}>
   <media-provider></media-provider>
   <media-video-layout></media-video-layout>
 </media-player>
