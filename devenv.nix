051512bbd656a3d8a37f9f08319b381b3ee785cf
dominic <dominicf2001@gmail.com>
2024-10-06 10:54:12 -0400

    envs

--- devenv.nix
+++ devenv.nix
@@ -1,15 +1,19 @@
 { pkgs, lib, config, inputs, ... }:
 
 {
-  packages = with pkgs; [ bun ];
+  packages = with pkgs; [ bun nodejs ];
 
   languages.dotnet = {
     enable = true;
     package = pkgs.dotnet-sdk_8;
   };
 
+  env.PROTOCOL_HEADER="x-forwarded-proto";
+  env.HOST_HEADER="x-forwarded-host";
+  env.ORIGIN="https://test.dominicferrando.com";
+
   processes = {
-  	front.exec = "bun ./frontend/build/index.js";
+  	# front.exec = "bun ./frontend/build/index.js";
   	back.exec = "cd ./backend; dotnet run";
   };
 
